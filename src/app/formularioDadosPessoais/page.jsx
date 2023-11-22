445f16a58addd7ace98d40e131032d0a75fecff9
dogzeira <douglasdanilobutu13@gmail.com>
2023-11-22 20:28:47 -0300

    Ajuste formulário dados pessoais

--- src/app/formularioDadosPessoais/page.jsx
+++ src/app/formularioDadosPessoais/page.jsx
@@ -16,29 +16,39 @@ export default function FormularioDadosPessoais() {
                   <input
                     type="text"
                     placeholder="Digite seu nome..."
-                    required="required"
+                    id="nome"
                   />
-                  <input type="text" placeholder="Digite seu sobrenome..." />
+                  <input
+                    type="text"
+                    placeholder="Digite seu sobrenome..."
+                    id="sobrenome"
+                  />
                 </div>
                 <div className="edicao-form">
-                  <input type="email" placeholder="Digite seu e-mail..." />
+                  <input
+                    type="email"
+                    placeholder="Digite seu e-mail..."
+                    id="email"
+                  />
                 </div>
                 <div className="edicao-form-2">
-                  <select name="genero">
+                  <select name="genero" id="genero">
+                    <option value="sexo">Selecione seu sexo!!</option>
                     <option value="feminino">Feminino</option>
                     <option value="masculino">Masculino</option>
                   </select>
-                  <input 
-                  type="date" 
-                  title="Digite sua data de nascimento..."
+                  <input
+                    type="date"
+                    title="Digite sua data de nascimento..."
+                    id="nascimento"
                   />
                 </div>
                 <div className="edicao-form">
                   <input
                     type="number"
                     placeholder="Digite seu cpf..."
-                    pattern="\d{3}\.\d{3}\.\d{3}-\d{2}"
                     title="Digite um CPF no formato: XXX.XXX.XXX-XX"
+                    id="cpf"
                   />
                 </div>
                 <div className="edicao-form">
@@ -46,13 +56,13 @@ export default function FormularioDadosPessoais() {
                     type="number"
                     placeholder="Digite seu número de telefone..."
                     title="Digite o telefone no formato: (XX) XXXXX-XXXX"
+                    id="telefone"
                   />
                 </div>
-                </form>
                 <Link href="/conteudoHome">
                   <button type="submit">Atualizar</button>
                 </Link>
-              
+              </form>
             </div>
           </div>
         </div>
