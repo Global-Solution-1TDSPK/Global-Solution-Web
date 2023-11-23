b517791a07db182092f538c54961556daa15bbe3
dogzeira <douglasdanilobutu13@gmail.com>
2023-11-23 18:58:02 -0300

    Criação de página conteudo home adm

---
+++ src/app/conteudoHomeAdm/page.jsx
@@ -0,0 +1,14 @@
+import Nav from "@/components/nav/Nav"
+import Footer from "@/components/footer/Footer"
+
+export default function ConteudoHomeAdm() {
+    return(
+        <>
+        <Nav />
+        <main>
+
+        </main>
+        <Footer />
+        </>
+    )
+}
